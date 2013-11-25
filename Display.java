05aab9b3e3fb1bb1e129f33434fc5ba4ff247518
chromestone <chromestone@users.noreply.github.com>
2013-11-25 18:39:15 -0500

    Update Display.java

--- Display.java
+++ Display.java
@@ -1,36 +1,39 @@
 import javax.swing.*;
 import javax.swing.event.*;
+import java.awt.FlowLayout;
 import java.awt.event.*;
+
 public class Display {
 	
 	private JFrame frame;
-	private JTextArea textArea;
+	private final JTextArea textArea;
 	
 	private final WebParser _parser;
 	private final Display _display;
 	
 	public Display(WebParser parser) {
 		frame = new JFrame("MCProhosting Query");
+		frame.setLayout(new FlowLayout());
 		frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-		frame.setSize(790, 450);
-		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
 
-		textArea = new JTextArea("Welcome to chromestone's MCProhosting Query", 50, 50);
+		textArea = new JTextArea("Welcome to chromestone's MCProhosting Query", 25, 50);
 		textArea.setEditable(false);
-
 		JScrollPane scrollPane = new JScrollPane(textArea);
-		JPanel panel = new JPanel();
-		panel.setOpaque(true);
-		panel.add(scrollPane);
+		frame.add(scrollPane);
+		
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.PAGE_AXIS));
+		buttonPanel.setOpaque(true);
 
-		JButton exitButton = new JButton("Exit");
-		exitButton.addMouseListener(new MouseInputAdapter() {
+		JButton clearButton = new JButton("Clear");
+		clearButton.addMouseListener(new MouseInputAdapter() {
 			public void mouseClicked(MouseEvent event) {
-				System.exit(0);
+				textArea.setText("Welcome to chromestone's MCProhosting Query");
 			}
 		});
-		panel.add(exitButton);
-
+		buttonPanel.add(clearButton);
+		
 		_parser = parser;
 		_display = this;
 		JButton queryButton = new JButton("Pull Info");
@@ -45,9 +48,20 @@ public class Display {
 				}
 			}
 		});
-		panel.add(queryButton);
-
-		frame.add(panel);
+		buttonPanel.add(queryButton);
+		
+		JButton exitButton = new JButton("Exit");
+		exitButton.addMouseListener(new MouseInputAdapter() {
+			public void mouseClicked(MouseEvent event) {
+				System.exit(0);
+			}
+		});
+		buttonPanel.add(exitButton);
+		
+		frame.add(buttonPanel);
+		
+		frame.pack();
+		frame.setLocationRelativeTo(null);
 	}
 	public void show() {
 		frame.setVisible(true);
