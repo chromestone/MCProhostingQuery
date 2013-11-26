4c0bc04ee1b2e5dbcbf1c4b13dfa16f3905461e1
chromestone <chromestone@users.noreply.github.com>
2013-11-26 17:27:12 -0500

    Update Display.java
    Easier to arrange buttons now.

--- Display.java
+++ Display.java
@@ -6,13 +6,14 @@ import java.awt.event.*;
 public class Display {
 	
 	private JFrame frame;
-	private final JTextArea textArea;
+	private JPanel buttonPanel;
 	
+	private final JTextArea textArea;
 	private final WebParser _parser;
 	private final Display _display;
 	
 	public Display(WebParser parser) {
-		frame = new JFrame("MCProhosting Query");
+		frame = new JFrame("MCProhosting Query 1.3.6");
 		frame.setLayout(new FlowLayout());
 		frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		frame.setResizable(false);
@@ -22,20 +23,35 @@ public class Display {
 		JScrollPane scrollPane = new JScrollPane(textArea);
 		frame.add(scrollPane);
 		
-		JPanel buttonPanel = new JPanel();
+		buttonPanel = new JPanel();
 		buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.PAGE_AXIS));
 		buttonPanel.setOpaque(true);
-
-		JButton clearButton = new JButton("Clear");
-		clearButton.addMouseListener(new MouseInputAdapter() {
-			public void mouseClicked(MouseEvent event) {
-				textArea.setText("Welcome to chromestone's MCProhosting Query");
-			}
-		});
-		buttonPanel.add(clearButton);
 		
 		_parser = parser;
 		_display = this;
+		createPullInfoButton();
+		
+		createClearButton();
+		
+		createExitButton();
+		
+		frame.add(buttonPanel);
+		
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+	}
+	
+	private void createExitButton() {
+		JButton exitButton = new JButton("Exit");
+		exitButton.addMouseListener(new MouseInputAdapter() {
+			public void mouseClicked(MouseEvent event) {
+				System.exit(0);
+			}
+		});
+		buttonPanel.add(exitButton);
+	}
+	
+	private void createPullInfoButton() {
 		JButton queryButton = new JButton("Pull Info");
 		queryButton.addMouseListener(new MouseInputAdapter() {
 			public void mouseClicked(MouseEvent event) {
@@ -49,31 +65,32 @@ public class Display {
 			}
 		});
 		buttonPanel.add(queryButton);
-		
-		JButton exitButton = new JButton("Exit");
-		exitButton.addMouseListener(new MouseInputAdapter() {
+	}
+	
+	private void createClearButton() {
+		JButton clearButton = new JButton("Clear");
+		clearButton.addMouseListener(new MouseInputAdapter() {
 			public void mouseClicked(MouseEvent event) {
-				System.exit(0);
+				textArea.setText("Welcome to chromestone's MCProhosting Query");
 			}
 		});
-		buttonPanel.add(exitButton);
-		
-		frame.add(buttonPanel);
-		
-		frame.pack();
-		frame.setLocationRelativeTo(null);
+		buttonPanel.add(clearButton);
 	}
+	
 	public void show() {
 		frame.setVisible(true);
 	}
+	
 	public void hide() {
 		frame.setVisible(false);
 	}
+	
 	public synchronized void append(String text) {
 		if (!text.isEmpty()) {
 			textArea.append("\n" + text);
 		}
 	}
+	
 	public void dispose() {
 		frame.dispose();
 	}
